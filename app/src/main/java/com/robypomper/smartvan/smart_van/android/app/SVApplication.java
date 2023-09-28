e7b3a8e762a63928068f31d01d1ba6b5803a5517
Roberto Pompermaier <robypomper@gmail.com>
2023-09-28 17:47:30 +0200

    feat(SVApplication): Updated the SVApplication and created the SVJSLClient according to the JSL application's support classes

--- app/src/main/java/com/robypomper/smartvan/smart_van/android/app/SVApplication.java
+++ app/src/main/java/com/robypomper/smartvan/smart_van/android/app/SVApplication.java
@@ -1,6 +1,19 @@
 package com.robypomper.smartvan.smart_van.android.app;
 
 import com.robypomper.josp.jsl.android.app.JSLApplication;
+import com.robypomper.josp.jsl.android.app.JSLClient;
+import com.robypomper.smartvan.smart_van.android.service.SVService;
 
-public class SVApplication extends JSLApplication {
+public class SVApplication extends JSLApplication<SVService> {
+
+    @Override
+    protected Class<? extends JSLClient<SVService>> getJSLClientClass() {
+        return SVJSLClient.class;
+    }
+
+    @Override
+    public SVJSLClient getJSLClient() {
+        return (SVJSLClient) super.getJSLClient();
+    }
+
 }
