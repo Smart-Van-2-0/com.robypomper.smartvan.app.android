3b55e3d85adb93f077505cae166ba8be9750fb3a
Roberto Pompermaier <robypomper@gmail.com>
2023-09-29 07:55:56 +0200

    feat(SVApplication): Added SVSelectObjectActivity as Smart Van objects selector
    Updated also the SVStartupActivity to redirect to the new SVSelectObjectActivity
    Created also the Definitions class to contain all SV definitions and constants
    Updated also SVStartupActivity to remove it from navigation history

--- app/src/main/java/com/robypomper/smartvan/smart_van/android/commons/SVDefinitions.java
+++ app/src/main/java/com/robypomper/smartvan/smart_van/android/commons/SVDefinitions.java
@@ -3,10 +3,16 @@ package com.robypomper.smartvan.smart_van.android.commons;
 import android.app.Activity;
 
 import com.robypomper.smartvan.smart_van.android.activities.SVMainActivity;
+import com.robypomper.smartvan.smart_van.android.activities.SVSelectObjectActivity;
 
 public class SVDefinitions {
 
+    /** JOD Smart Van object's model used to filter available JOD Objects. */
+    public static final String MODEL_NAME = "JOD Smart Van";
+
     /** Define which activity must be show after the SVStartupActivity detect the JSL initialization. */
-    public static final Class<? extends Activity> NEXT_ACTIVITY_STARTUP = SVMainActivity.class;
+    public static final Class<? extends Activity> NEXT_ACTIVITY_STARTUP = SVSelectObjectActivity.class;
+    /** Define which activity must be show after the SVSelectObjectActivity selected an object. */
+    public static final Class<? extends Activity> NEXT_ACTIVITY_SELECTOBJECT = SVMainActivity.class;
 
 }
