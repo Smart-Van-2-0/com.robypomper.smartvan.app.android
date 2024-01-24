f519824aa77e7e8e6167c6a95f6b17f177cde4cd
Roberto Pompermaier <robypomper@gmail.com>
2024-01-24 15:25:45 +0100

    fix(Charts): Minor fix on logging messages

--- chart_views/src/main/java/com/robypomper/josp/jsl/android/components/charts/adapters/JSLChartViewAdapter.java
+++ chart_views/src/main/java/com/robypomper/josp/jsl/android/components/charts/adapters/JSLChartViewAdapter.java
@@ -105,10 +105,10 @@ public class JSLChartViewAdapter extends ChartViewAdapterAbs {
                     );
                     Log.d("JSLChartViewAdapter", "DataSet '" + dataSetName + "': doFetch-TX from " + ChartLineView.LOG_SDF.format(historyLimits.getFromDate()) + " to " + ChartLineView.LOG_SDF.format(historyLimits.getToDate()));
                 } catch (JSLRemoteObject.ObjectNotConnected e) {
-                    Log.d("JSLChartViewAdapter", "Can't fetch data set '" + dataSetName + "' because the object '" + dataSetComp.getRemoteObject().getName() + "' is not connected.");
+                    Log.w("JSLChartViewAdapter", "Can't fetch data set '" + dataSetName + "' because the object '" + dataSetComp.getRemoteObject().getName() + "' is not connected.");
                     // let timeout the fetch process, it can be improved
                 } catch (JSLRemoteObject.MissingPermission e) {
-                    Log.d("JSLChartViewAdapter", "Can't fetch data set '" + dataSetName + "' because missing required permission on the object '" + dataSetComp.getRemoteObject().getName() + "' is not connected.");
+                    Log.w("JSLChartViewAdapter", "Can't fetch data set '" + dataSetName + "' because missing required permission on the object '" + dataSetComp.getRemoteObject().getName() + "' is not connected.");
                     // let timeout the fetch process, it can be improved
                 }
             }
