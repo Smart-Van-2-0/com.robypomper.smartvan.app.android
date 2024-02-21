203adf497d3d1fab87be74f187f0bfa6cf61160a
Roberto Pompermaier <robypomper@gmail.com>
2024-02-21 15:16:24 +0100

    feat(SVMain): Added SVSubStorage SVAbsSubStorage as base for SV Storage sub-components

---
+++ app/src/main/java/com/robypomper/smartvan/smart_van/android/storage/SVAbsSubStorage.java
@@ -0,0 +1,45 @@
+package com.robypomper.smartvan.smart_van.android.storage;
+
+/**
+ * This class is used to define the abstract implementation of a sub storage.
+ */
+public abstract class SVAbsSubStorage implements SVSubStorage {
+
+    /**
+     * The objectId of the storage.
+     */
+    private String objectId = null;
+
+
+    /**
+     * This method is used to check if the storage is generated.
+     *
+     * @return true if the storage is generated, false otherwise.
+     */
+    protected boolean isStorageGenerated() {
+        return objectId != null;
+    }
+
+    /**
+     * This method is used to get the objectId of the storage.
+     *
+     * @return the objectId of the storage.
+     */
+    @Override
+    public String getObjectId() {
+        return objectId;
+    }
+
+    /**
+     * This method is used to set the objectId of the storage.
+     * <p>
+     * The object id can be set only from a subclass during the generation of
+     * the storage.
+     *
+     * @param objectId the objectId of the storage.
+     */
+    protected void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+}
